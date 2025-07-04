f9400df3ab051cdce95941d629b0e19551eaba5e
OgneFF <wyrik9324@gmail.com>
2025-07-04 22:10:24 +0300

    Добавлен Апи кей и роут для админки

--- src/middleware/veridyApiKey.ts
+++ src/middleware/veridyApiKey.ts
@@ -3,8 +3,11 @@ import { Request, Response, NextFunction } from "express";
 export function verifyApiKey(req: Request, res: Response, next: NextFunction) {
   const keyFromRequest = req.headers["x-api-key"];
   const validKey = process.env.ADMIN_KEY;
+  console.log(keyFromRequest);
+  console.log(validKey);
 
   if (!keyFromRequest || keyFromRequest !== validKey) {
+    console.log("we are here");
     res.status(403).json({ error: "Forbidden. Invalid API key." });
     return;
   }
